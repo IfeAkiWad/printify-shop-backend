1556b2c3fc52d6c7aaea67c666521187e6c3638b
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-07-17 22:13:43 -0500

    create app  controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload)
+      end
+    
+      def auth_header
+        request.headers['Authorization']
+      end
+    
+      def decoded_token
+        if auth_header
+          token = auth_header.split(' ')[1]
+          begin
+            JWT.decode(token, true, algorithm: 'RS256')
+          rescue JWT::DecodeError
+            nil
+          end
+        end
 end
